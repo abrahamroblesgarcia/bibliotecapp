858c3b8aef8ecff1962a2f2e809283d5e74238c9
Abraham <abraham.robles@sidn.es>
2021-11-20 18:09:07 +0100

    creadas las migraciones para la db

--- database/migrations/2021_11_20_162422_create_table_authors.php
+++ database/migrations/2021_11_20_162422_create_table_authors.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableAutors extends Migration
+class CreateTableAuthors extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,12 @@ class CreateTableAutors extends Migration
      */
     public function up()
     {
-        Schema::create('table_autors', function (Blueprint $table) {
+        Schema::create('authors', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name',100);
+            $table->string('pseudonym',100);
+            $table->date('birth_date');
+            $table->date('death_date');
         });
     }
 
@@ -26,6 +29,6 @@ class CreateTableAutors extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_autors');
+        Schema::dropIfExists('authors');
     }
 }
